cc4f6dde0678cee032bd44d770ec371958285de9
Dev Kapadia <devkapadia123@gmail.com>
2024-06-22 11:44:01 +0530

    student's getAllStudent and getStudentby id is done

---
+++ controllers/studentController.js
@@ -0,0 +1,25 @@
+const Student = require('../model/studentSchema');
+
+// Get all students
+exports.getAllStudents = async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.status(200).json(students);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching students', error });
+    }
+};
+
+// Get student by ID
+exports.getStudentById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const student = await Student.findById(id);
+        if (!student) {
+            return res.status(404).json({ message: 'Student not found' });
+        }
+        res.status(200).json(student);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching student', error });
+    }
+};
---
+++ router/studentRoute.js
@@ -0,0 +1,13 @@
+// universityRoutes.js
+const express = require('express');
+const router = express.Router();
+const studentController = require('../controllers/studentController');
+const { verifyToken } = require('../middleware/middlewares');
+
+// GET all universities
+router.get('/students', verifyToken, studentController.getAllStudents);
+
+// GET a university by ID
+router.get('/students/:id', verifyToken, studentController.getStudentById);
+
+module.exports = router;
