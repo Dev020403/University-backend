1325617506abc7fe94696fb80ba3bdcf7b51313a
Dev Kapadia <devkapadia123@gmail.com>
2024-06-22 12:44:30 +0530

    update student details is done

--- router/studentRoute.js
+++ router/studentRoute.js
@@ -10,4 +10,7 @@ router.get('/students', verifyToken, studentController.getAllStudents);
 // GET a university by ID
 router.get('/students/:id', verifyToken, studentController.getStudentById);
 
+// Update student details
+router.put('/update-student/:_id', studentController.updateStudentDetails);
+
 module.exports = router;
