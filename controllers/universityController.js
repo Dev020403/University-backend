cbf0859d97ad9adb94469a0d59c521d302bd21f4
Dev Kapadia <devkapadia123@gmail.com>
2024-06-25 14:33:56 +0530

    update university route is created

--- controllers/universityController.js
+++ controllers/universityController.js
@@ -28,8 +28,25 @@ const getUniversityById = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+const updateUniversity = async (req, res) => {
+    const { _id } = req.params;
+    const updateData = req.body;
+
+    try {
+        const updatedUniversity = await University.findByIdAndUpdate(_id, updateData, { new: true, runValidators: true });
+
+        if (!updatedUniversity) {
+            return res.status(404).json({ message: 'University not found' });
+        }
+
+        res.json(updatedUniversity);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
 
 module.exports = {
     getAllUniversity,
     getUniversityById,
+    updateUniversity
 };
--- router/univesityRoute.js
+++ router/univesityRoute.js
@@ -10,4 +10,7 @@ router.get('/universities', verifyToken, universityController.getAllUniversity);
 // GET a university by ID
 router.get('/universities/:id', verifyToken, universityController.getUniversityById);
 
+// PUT update a university by ID
+router.put('/update-university/:_id', verifyToken, universityController.updateUniversity);
+
 module.exports = router;
