07db06adcdcb2b1f2cf5bcd84467c3b0f862611e
Dev Kapadia <devkapadia123@gmail.com>
2024-06-26 15:30:37 +0530

    merge complete with student

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const connectDb = require('./Config/connectDB');
 const bodyParser = require('body-parser');
 const routes = require('./router/routes');
 const universityRoutes = require('./router/univesityRoute');
+const courseRoutes = require('./router/courseRoute');
+const applicationRoutes = require('./router/applicationRoute');
+
+const studentRoutes = require('./router/studentRoute');
+
 
 const app = express();
 const port = 3000;
@@ -18,6 +23,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 app.use('/api', universityRoutes);
+app.use('/api', courseRoutes);
+app.use('/api', applicationRoutes);
+app.use('/api', studentRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
